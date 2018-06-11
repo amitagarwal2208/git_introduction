c93b4b66c51bfdcbb0368ee70bea38852b29c8c3
Amit Agarwal <amitagarwal@Amits-MacBook-Air.local>
2018-06-11 16:48:57 +0530

    sheldon greeter

--- file01.js
+++ file01.js
@@ -1,7 +1,9 @@
 console.log('Hello World! We are modified!')
 
 function greet(name){
-    console.log('hello '+name)
+    console.log('knock knock '+name + ' how are you?')
+    console.log('knock knock '+name + ' how are you?')
+    console.log('knock knock '+name + ' how are you?')
 }
 
 greet('AMIT')
